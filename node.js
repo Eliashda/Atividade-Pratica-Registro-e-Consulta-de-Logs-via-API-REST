7897e05aa8909a7a1ce8e06c1f4c74ec7c837c3d
ELIAS ALMENDA RODRIGUES PADILHA <elias_a_padilha@estudante.sesisenai.org.br>
2025-05-07 09:35:35 -0300

    criação do uuid

--- node.js
+++ node.js
@@ -2,29 +2,49 @@
 import { rejects } from 'assert'
 import express from 'express'
 import fs from 'fs'
+import { v4 as uuidv4 } from 'uuid'
 const app = express()
 const PORT = 8000
+app.use(express.text())
 
-function readFile(filepath){
-    return new Promise((resolve, reject) =>{
-        fs.readFile(filepath,data, 'utf-8', (err)=>{
+function readFile(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, data, 'utf-8', (err) => {
             if (err) return reject(err)
-                resolve(data)
+            resolve(data)
         })
     }
 
     )
 }
 
-function writeFile(filepath){
-    return new Promise((resolve, reject) =>{
-        fs.writeFile(filepath,data, 'utf-8', (err)=>{
+function writeFile(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filepath, data, 'utf-8', (err) => {
             if (err) return reject(err)
-                resolve(null)
+            resolve(null)
         })
     }
 
     )
 }
+const logsPath = "logs.txt"
+async function newLog(name) {
+    const date = Date.now()
+    const uuid = uuidv4()
+    const log = `${uuid}- ${date}- ${name}`
 
-app.get()
+    const allLogs = await readFile(logsPath)
+    await writeFile(logsPath, allLogs + "\n" + log)
+    return log
+}
+app.post('logs', (req, res) => {
+    if (!req.body) return res.status(400).send("voce ja passou seu nome no corpo")
+
+    newLog(req.body).then(log => {
+        res.status(204).send()
+    }).catch(err =>
+        res.status(500).send(err)
+    )
+}
+)
